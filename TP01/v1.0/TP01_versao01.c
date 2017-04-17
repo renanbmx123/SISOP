778ae0541d77c94c5c85673a42cc2d95806d2558
MLongarai <mlongarai@outlook.com>
2017-04-17 11:01:12 -0300

    Em desenvolvimento

--- TP01/v1.0/TP01_versao01.c
+++ TP01/v1.0/TP01_versao01.c
@@ -2,47 +2,47 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define CADEIRAS 5
-#define TIMEWAIT_MAX 5
+#define CADEIRAS 5              /* Numero de cadeiras*/
+#define TIMEWAIT_MAX 5          /* Tempo maximo de espera */
 
-typedef enum {
+typedef enum {                  /* Definicao das etapas dos filosofos */    
     pensar,
     fome,
-    comer, eat
+    comer,
 } Filosofos;
 
-int Filosofos_esquerda(int filo_n);
+int Filosofos_esquerda(int filo_n);     
 int Filosofos_direita(int filo_n);
 void *Filosofos_checkin(void *filo);
 
-pthread_mutex_t forks;
+pthread_mutex_t forks;                  /* exclusao mutua para regioes cri­ticas */
 Filosofos *FilosofosN;
 
 int main(){
 
-    pthread_t *thread;
+    pthread_t *thread;                              
 
-    FilosofosN = (Filosofos *) calloc(CADEIRAS, sizeof(Filosofos));
+    FilosofosN = (Filosofos *) calloc(CADEIRAS, sizeof(Filosofos));     /* Colocando os filosofos nas cadeiras */
     if(FilosofosN == 0){
-        printf("\nErro ao alocar os filosofos!\n");
+        printf("\nErro ao alocar os filosofos!\n");                     /* Se nao tiver nenhum filosofo setado, retorna erro */
         return -1;
     }
 
-    thread = (pthread_t *) calloc(CADEIRAS, sizeof(pthread_t));
+    thread = (pthread_t *) calloc(CADEIRAS, sizeof(pthread_t));         /* Aloca uma thread para cada filosofo */
     if(thread == 0){
-        printf("\nErro ao alocar as threads!\n");
+        printf("\nErro ao alocar as threads!\n");                       /* Se nao tiver nenhuma thread, retorna erro */
         return -1;
     }
 
-    pthread_mutex_init(&forks, NULL);
+    pthread_mutex_init(&forks, NULL);                                   /* Inicializa regiao cri­tica */
 
     int i;
-    for(i=0; i<CADEIRAS; i++){
+    for(i=0; i<CADEIRAS; i++){                                          /* Todos os filosofos em estado PENSANDO */
         FilosofosN[i] = pensar;
         pthread_create(&thread[i], NULL, Filosofos_checkin, (void *) i);
     }
 
-    for(i=0; i<CADEIRAS; i++){
+    for(i=0; i<CADEIRAS; i++){                                          /* Todas as cadeiras com as threads */
         pthread_join(thread[i], NULL);
     }
 
@@ -50,23 +50,23 @@ int main(){
 }
 
 int Filosofos_esquerda(int filo_n){
-    return (filo_n + CADEIRAS - 1) % CADEIRAS;
+    return (filo_n + CADEIRAS - 1) % CADEIRAS;          /* Devolve o garfo esquerdo <<< a mesa */
 }
 
 int Filosofos_direita(int filo_n){
-    return (filo_n + 1) % CADEIRAS;
+    return (filo_n + 1) % CADEIRAS;                     /* Devolve o garfo direito >>> a mesa */
 }
 
 void *Filosofos_checkin(void *filo){
     int filo_n = (int) filo;
 
-    while(1){
+    while(1){                                           /* Switch case para garantir que o filosofo soh ira comer se ambos os garfos estiverem liberados */
         pthread_mutex_lock(&forks);
 
         switch(FilosofosN[filo_n]){
             case pensar:
                 FilosofosN[filo_n] = fome;
-                pthread_mutex_unlock(&forks);
+                pthread_mutex_unlock(&forks);           /* Destrava semafaro para o filosofo que está pensando para passar ao passo de COMER...*/
 
                 printf("Filosofos #%d: pensando...\n", filo_n);
 
@@ -75,7 +75,7 @@ void *Filosofos_checkin(void *filo){
 
             case comer:
                 FilosofosN[filo_n] = pensar;
-                pthread_mutex_unlock(&forks);
+                pthread_mutex_unlock(&forks);           /* Destrava semafaro para o filosofo que está comendo para passar ao passo de PENSAR...*/
 
                 printf("Filosofos #%d: Parando de comer, voltando a pensar...\n", filo_n);
 
@@ -86,14 +86,14 @@ void *Filosofos_checkin(void *filo){
                 printf("Filosofos #%d: estou com fome...\n", filo_n);
 
                 if(FilosofosN[Filosofos_esquerda(filo_n)] == comer){
-                    pthread_mutex_unlock(&forks);
-                    printf("Filosofos #%d:       Filosofos #%d (esquerda) esta comendo...\n", filo_n, Filosofos_esquerda(filo_n));
+                    pthread_mutex_unlock(&forks);                           /* Destrava semafaro para o filosofo que está TENTANDO comer SE o garfo da ESQUERDA estiver liberado...*/
+                    printf("Filosofos #%d:       Tentando comer mas o folosofo %d que esta na ESQUERDA esta comendo...\n", filo_n, Filosofos_esquerda(filo_n));
                 } else if(FilosofosN[Filosofos_direita(filo_n)] == comer){
-                    pthread_mutex_unlock(&forks);
-                    printf("Filosofos #%d:       Filosofos #%d (direita) esta comendo...\n", filo_n, Filosofos_direita(filo_n));
+                    pthread_mutex_unlock(&forks);                           /* Destrava semafaro para o filosofo que está TENTANDO comer SE o garfo da DIREITA estiver liberado...*/
+                    printf("Filosofos #%d:       Tentando comer mas o folosofo %d que esta na DIREITA esta comendo...\n", filo_n, Filosofos_direita(filo_n));
                 }else{
                     FilosofosN[filo_n] = comer;
-                    pthread_mutex_unlock(&forks);
+                    pthread_mutex_unlock(&forks);                           /* Destrava semafaro se os garfos estiverem ambos os garfos estiverem liberados... */
                     printf("Filosofos #%d:       Comendo!\n", filo_n);
                 }
 
